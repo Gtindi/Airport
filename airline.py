7ac3fb5016319398f913be95b508fb32fc158af9
Gtindi <tindigabriel@gmail.com>
2021-11-05 00:53:48 +0300

    initial commit

--- airline.py
+++ airline.py
@@ -1,18 +1,21 @@
+import matplotlib
 import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-# plt.figure(figsize=(12,8))
-# nx.draw_networkx(df, with_labels=True)
 dp = pd.read_csv('data.csv')
-print(dp)
+# print(dp)
 
-df = nx.from_pandas_edgelist(data, source='Origin', target='Dest', edge_attr=True)
-# df.nodes()
+df = nx.from_pandas_edgelist(dp, source='Origin', target='Dest', edge_attr=True)
+print(df.nodes)
+print(df.edges)
 # df.edges()
-# # % matplotlib inline
+
+
+plt.figure(figsize=(12,8))
+x = nx.draw_networkx(df, with_labels=True)
+print(plt.show())
 # shortest_path_distance = nx.dijkstra_path(df, source='AMA', target='PBI', weight='Distance')
 # Shortest_path_distance
 
